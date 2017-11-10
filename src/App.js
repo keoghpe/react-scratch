047c434f3be42bc794c9b0a02ed619996dbf0fed
keoghpe@tcd.ie <keoghpe@tcd.ie>
2017-11-10 14:16:42 +0000

    Clean up and add merch

--- src/App.js
+++ src/App.js
@@ -25,17 +25,96 @@ class SocialIcons extends Component {
   }
 }
 
+// class Tabs extends Component {
+//   render() {
+//     return (
+//       <nav>
+//         <ul>
+//       {this.props.tabList.map(function(tab) {
+//         return (
+//           <Tab url={tab.url} name={tab.name} />
+//         )
+//       })}
+//       </ul>
+//       </nav>
+//     )
+//   }
+// }
+//
+// class Tab extends Component {
+//   render() {
+//     return (
+//         <li></li>
+//     )
+//   }
+// }
+
+// var tabList = [
+//     { 'id': 1, 'name': 'Mike', 'url': '/mike' },
+//     { 'id': 2, 'name': 'Donnie', 'url': '/donnie' },
+//     { 'id': 3, 'name': 'Raph', 'url': '/raph' },
+//     { 'id': 4, 'name': 'Leo', 'url': '/leo' }
+// ];
+
+class VideoSection extends Component {
+  render() {
+    return (
+      <div>
+      <YouTube
+      videoId="NMXbfL8vLvk"
+      onReady={this._onReady}
+      />
+      </div>
+    )
+  }
+
+  _onReady(event){
+    event.target.playVideo();
+  }
+}
+
+// class VideoTab extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick() {
+//     alert('hello from ' + this.props.tabName);
+//   }
+//
+//   render() {
+//     return (
+//       <div class="tab" onClick={this.handleClick}>
+//       <span>{this.props.tabName}</span>
+//       </div>
+//     )
+//   }
+// }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <section>
       <SocialIcons />
-      <h2><a href="https://thescratchmerch.bigcartel.com">Buy some scratchy merch</a></h2>
+      </section>
+      <section className="merchSection">
+      <a href="https://thescratchmerch.bigcartel.com">
+      <h2>Buy some scratchy merch</h2>
+      <img src="merch-image.jpg" />
+      <h2>Buy some scratchy merch</h2>
+      </a>
+      </section>
+      <section>
+      <VideoSection />
+      </section>
+      <section>
       <SpotifyPlayer
-      uri="spotify:artist:6BCpGC485tSshwjemmeAmd"
+      uri="spotify:user:thescratchmusic:playlist:5mJfZYjTaFgElyLbGMuaxv"
       theme="white"/>
-      <YouTube videoId="NMXbfL8vLvk"/>
-      <YouTube videoId="tA-lXi_SFvo"/>
+      </section>
       </div>
     );
   }
