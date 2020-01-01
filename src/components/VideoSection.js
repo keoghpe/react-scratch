43fe04b968ceb429db6d4ed30217747040d8cb90
keoghpe@tcd.ie <keoghpe@tcd.ie>
2020-01-01 23:22:39 +0000

    Add videos

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,21 +1,81 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import YouTube from 'react-youtube';
 
-class VideoSection extends Component {
-  render() {
-    return (
-      <div>
-      <YouTube
-      videoId="dfKuN7SJiKc"
-      onReady={this._onReady}
-      />
-      </div>
-    )
+const VideoList = [
+  {
+    id: 'dfKuN7SJiKc',
+    title: 'God Slap'
+  },
+  {
+    id: '7XnpHMfoA3s',
+    title: 'Head in the Sleeve'
+  },
+  {
+    id: 'BcEIoDxzHg4',
+    title: 'Cig in the Breeze'
+  },
+  {
+    id: 't4CKZG2exVw',
+    title: 'Old Dog'
+  },
+  {
+    id: '2QWwnlvO-HQ',
+    title: 'Cunla'
+  },
+  {
+    id: 'dbqNKNkL4R4',
+    title: 'Flaker'
+  },
+  {
+    id: 'pUwUbz220xA',
+    title: 'The Road to Ballyshannon'
+  },
+  {
+    id: 'cjCe9lTMzLA',
+    title: 'Shadow ft. Loah'
+  },
+  {
+    id: 'NMXbfL8vLvk',
+    title: 'Punisher'
+  },
+  {
+    id: 'tA-lXi_SFvo',
+    title: 'Get It Right Up Ye'
+  },
+  {
+    id: 'escYjNE0YMo',
+    title: 'Vico Road'
   }
+];
 
-  _onReady(event){
-    event.target.playVideo();
-  }
-}
+const VideoImage = ({id, title, setActiveVideo}) => (
+  <div className="videoImage" onClick={() => setActiveVideo(id)}>
+    <h4>{title}</h4>
+    <div className="image" style={{backgroundImage: `url(https://img.youtube.com/vi/${id}/0.jpg)`}}>
+      <i className="fa fa-play-circle"></i>
+    </div>
+  </div>
+);
+
+const VideoModal = ({activeVideo, setActiveVideo}) => (
+  <div className="videoModal" onClick={(e) => {setActiveVideo(null)}}>
+    <i className="fa fa-times"></i>
+    <YouTube
+      videoId={activeVideo}
+      onReady={(event) => event.target.playVideo()}
+    />
+  </div>
+);
+
+const VideoSection = () => {
+  const [activeVideo, setActiveVideo] = useState(null);
+
+  return (
+    <div>
+      {VideoList.map((props) => <VideoImage {...props} setActiveVideo={setActiveVideo}/>)}
+      {activeVideo && <VideoModal activeVideo={activeVideo} setActiveVideo={setActiveVideo}/>}
+    </div>
+  )
+};
 
 export default VideoSection;
