aa68f685adda1e56de4d54d09b71d02c8f887ad3
Peter Keogh <peterkeoghdev@gmail.com>
2020-05-04 00:04:42 +0100

    Update gigs

--- src/components/GigList.js
+++ src/components/GigList.js
@@ -11,90 +11,40 @@ const GigList = () => {
             onSale: true
         },
         {
-            date: new Date(2020, 4, 23),
+            date: new Date(2020, 11, 18),
             venue: 'The Academy',
             location: 'Dublin',
             link: 'https://www.ticketmaster.ie/the-scratch-dublin-10-04-2020/event/1800578DCDFDF5AA',
             onSale: true
         },
         {
-            date: new Date(2020, 4, 29),
+            date: new Date(2020, 11, 19),
             venue: 'Limelight 2',
             location: 'Belfast',
             link: 'https://www.ticketmaster.ie/the-scratch-belfast-11-04-2020/event/3800578DD2A81F42?fbclid=IwAR2miTCwQycwSSnkX0wuDzmHNFcH6oK85k1PkKhmLB3Po09iRnFNl2Z8-gI',
             onSale: true
         },
         {
-            date: new Date(2020, 5, 4),
+            date: new Date(2020, 11, 4),
             venue: 'Cyprus Avenue',
             location: 'Cork',
             link: 'https://www.eventbrite.ie/e/the-scratch-tickets-86209634337?fbclid=IwAR3yc9MctYKhy6g3d6I1T4KlYbA2jguUdZ0DXoPqN03oklz18fvEs5IDv9M',
             onSale: true
         },
         {
-            date: new Date(2020, 4, 21),
+            date: new Date(2020, 11, 10),
             venue: 'Roisin Dubh',
             location: 'Galway',
             link: 'https://www.roisindubh.net/gig-details.html?listingID=10500&fbclid=IwAR2K9rXrvM9A0kDimWef8L7HoeDklWOBM9g70zhljY1oVTVRzSSH7IFVg4g',
             onSale: true
         },
         {
-            date: new Date(2020, 4, 29),
+            date: new Date(2020, 11, 12),
             venue: 'Dolans',
             location: 'Limerick',
             link: 'https://dolans.yapsody.com/event/index/505651/the-scratch?fbclid=IwAR29N57saKpDKtnoxz7HWL6zlamfce6KiEwX-XqrLpRaRW4sYBD_msChRGc',
             onSale: true
-        },
-        {
-            date: new Date(2020, 4, 12),
-            venue: 'King tut\'s wah wah hut',
-            location: 'Glasgow',
-            link: 'https://www.ticketmaster.co.uk/the-scratch-tickets/artist/5334928',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 4, 13),
-            venue: 'Star & Garter',
-            location: 'Manchester',
-            link: 'https://www.ticketmaster.co.uk/the-scratch-tickets/artist/5334928',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 4, 14),
-            venue: 'The Great Escape',
-            location: 'Brighton',
-            link: 'http://bit.ly/TGEFEST20',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 4, 15),
-            venue: 'Boston Music Room',
-            location: 'London',
-            link: 'https://www.ticketmaster.co.uk/the-scratch-tickets/artist/5334928',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 4, 16),
-            venue: 'The Great Escape',
-            location: 'Brighton',
-            link: 'http://bit.ly/TGEFEST20',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 4, 31),
-            venue: 'Saltwater Festival',
-            location: 'Westport',
-            link: 'https://www.ticketmaster.ie/saltwater-festival-tickets/artist/5324131',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 5, 14),
-            venue: 'Sunstroke Festival',
-            location: 'Punchestown Racecourse',
-            link: 'https://www.sunstrokeireland.com/tickets',
-            onSale: true
-        },
-    ]
+        }]
 
     return gigListData.length ? gigListData
         .sort((a, b) => a.date.getTime() - b.date.getTime())
