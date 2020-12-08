f3a4fa99b8e77439457e139185be1b43e28b87b1
Peter Keogh <peterkeoghdev@gmail.com>
2020-12-08 19:53:54 +0000

    Update gigs

--- src/components/GigList.js
+++ src/components/GigList.js
@@ -4,47 +4,48 @@ import moment from 'moment';
 const GigList = () => {
     const gigListData = [
         {
-            date: new Date(2020, 0, 25),
-            venue: 'De Barra\'s',
-            location: 'Clonakilty',
-            link: 'https://www.eventbrite.ie/e/the-scratch-live-in-de-barras-rescheduled-tickets-61109974579',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 11, 18),
-            venue: 'The Academy',
+            date: new Date(2021, 4, 28),
+            venue: 'Olympia Theatre',
             location: 'Dublin',
-            link: 'https://www.ticketmaster.ie/the-scratch-dublin-10-04-2020/event/1800578DCDFDF5AA',
+            link: 'https://www.ticketmaster.ie/venueartist/198239/5263810',
             onSale: true
         },
         {
-            date: new Date(2020, 11, 19),
-            venue: 'Limelight 2',
-            location: 'Belfast',
-            link: 'https://www.ticketmaster.ie/the-scratch-belfast-11-04-2020/event/3800578DD2A81F42?fbclid=IwAR2miTCwQycwSSnkX0wuDzmHNFcH6oK85k1PkKhmLB3Po09iRnFNl2Z8-gI',
-            onSale: true
-        },
-        {
-            date: new Date(2020, 11, 4),
+            date: new Date(2021, 4, 29),
             venue: 'Cyprus Avenue',
             location: 'Cork',
-            link: 'https://www.eventbrite.ie/e/the-scratch-tickets-86209634337?fbclid=IwAR3yc9MctYKhy6g3d6I1T4KlYbA2jguUdZ0DXoPqN03oklz18fvEs5IDv9M',
+            link: 'https://www.eventbrite.ie/e/the-scratch-tickets-86209634337',
             onSale: true
         },
         {
-            date: new Date(2020, 11, 10),
+            date: new Date(2021, 4, 20),
             venue: 'Roisin Dubh',
             location: 'Galway',
-            link: 'https://www.roisindubh.net/gig-details.html?listingID=10500&fbclid=IwAR2K9rXrvM9A0kDimWef8L7HoeDklWOBM9g70zhljY1oVTVRzSSH7IFVg4g',
+            link: 'https://www.ticketmaster.ie/the-scratch-galway-20-05-2021/event/1800582DA80653BB',
             onSale: true
         },
         {
-            date: new Date(2020, 11, 12),
+            date: new Date(2021, 4, 23),
+            venue: 'The Limelight 2',
+            location: 'Belfast',
+            link: 'https://www.ticketmaster.ie/the-scratch-belfast-19-12-2020/event/3800578DD2A81F42',
+            onSale: true
+        },
+        {
+            date: new Date(2021, 4, 22),
             venue: 'Dolans',
             location: 'Limerick',
-            link: 'https://dolans.yapsody.com/event/index/505651/the-scratch?fbclid=IwAR29N57saKpDKtnoxz7HWL6zlamfce6KiEwX-XqrLpRaRW4sYBD_msChRGc',
+            link: 'https://dolans.yapsody.com/event/index/505651/the-scratch',
             onSale: true
-        }]
+        },
+        {
+            date: new Date(2021, 4, 21),
+            venue: 'Mike the Pies',
+            location: 'Listowel',
+            link: 'https://mikethepies.com/the-scratch/',
+            onSale: true
+        },
+    ];
 
     return gigListData.length ? gigListData
         .sort((a, b) => a.date.getTime() - b.date.getTime())
