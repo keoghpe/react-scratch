5bb9b1d9649448a30b5af74ef4296ddee90cdd30
Peter Keogh <peter.keogh@shopify.com>
2021-09-18 20:03:21 +0100

    Add new assets for album

--- src/App.js
+++ src/App.js
@@ -1,81 +1,91 @@
-import React, {useState, useEffect} from 'react';
-import YouTube from 'react-youtube';
-import SpotifyPlayer from 'react-spotify-player';
-import './App.scss';
-import Bird from './components/Bird';
-import VideoSection from './components/VideoSection';
-import GigList from './components/GigList';
-import {Switch, Route, Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import SpotifyPlayer from "react-spotify-player";
+import "./App.scss";
+import Bird from "./components/Bird";
+import VideoSection from "./components/VideoSection";
+import GigList from "./components/GigList";
+import { Switch, Route, Link } from "react-router-dom";
 import StreamingIconList from "./components/StreamingIconList";
 import IconLink from "./components/IconLink";
-import {imageForVideo, VideoList} from "./components/VideoSection";
+import { imageForVideo, VideoList } from "./components/VideoSection";
 
 const Home = () => (
   <section className="albumSection">
     <div className="albumImage">
-      <img
-        srcSet="Album-Cover-200.jpg 200w, Album-Cover-600.jpg 600w, Album-Cover-1000.jpg 1000w"
-        sizes="(max-width: 600px) 200px, (max-width: 1200px) 600px, 1000px"
-        src="/Album-Cover-1000.jpg"
-        alt=""/>
+      {/* <img
+        // srcSet="Album-Cover-200.jpg 200w, Album-Cover-600.jpg 600w, Album-Cover-1000.jpg 1000w"
+        // sizes="(max-width: 600px) 200px, (max-width: 1200px) 600px, 1000px"
+        // src="/Album-Cover-1000.jpg"
+        // src="/Album-Cover-1000.jpg"
+        src="/FrontCoverAsset.png"
+        alt="/FrontCoverAsset.png"/> */}
+
+      <img src="/FrontCoverAsset.png" alt="/FrontCoverAsset.png" />
+      {/* <img
+        src="/BackCoverAsset.png"
+        alt="/FrontCoverAsset.png"/> */}
     </div>
     <div className="albumText">
       <div className="textCollection">
-        <h2>&lsquo;Couldn't Give a Rats&rsquo;</h2>
-        <h3 className="emphasised">The debut album!</h3>
-        <h3>Out now on all streaming platforms.</h3>
-        <StreamingIconList/>
+        <h2>
+          Couldn't Leave Our Gafs <br /> (Live 2021)
+        </h2>
+        <h3 className="emphasised">Available for pre-order <br />
+        €25 + p &amp; p
+        </h3>
+        <div id="product-component-1631916191603"></div>
       </div>
     </div>
   </section>
-)
+);
 
 const Gigs = () => (
   <section className="gigSection">
     <table>
-      <GigList/>
+      <GigList />
     </table>
-  </section>
-)
-
-const Merchandise = () => (
-  <section>
-
-  </section>
-)
-
-const Merch = () => {
-  window.location.href = "https://thescratchmerch.bigcartel.com"
-}
-
-const ARTIST_ID = '6BCpGC485tSshwjemmeAmd';
-
-const Music = () => (
-  <section className="musicSection">
-    <iframe src={`https://open.spotify.com/follow/1/?uri=spotify:artist:${ARTIST_ID}&size=detail&theme=dark`}
-            className={'spotifyFollow'}
-            scrolling="no"
-            frameBorder="0" style={{border: 'none', overflow: 'hidden'}}
-            allowTransparency="true"></iframe>
-    <SpotifyPlayer
-      theme="white"
-      view="coverart"
-      uri={`spotify:album:3MctXvWGZKZQDmSheN8Cgo`}/>
   </section>
 );
 
-const initialLocation = window.location.href.split('/').pop();
+const Merchandise = () => <section></section>;
+
+const Merch = () => {
+  window.location.href = "https://thescratchmerch.bigcartel.com";
+};
+
+const ARTIST_ID = "6BCpGC485tSshwjemmeAmd";
+
+const Music = () => (
+  <section className="musicSection">
+    <iframe
+      src={`https://open.spotify.com/follow/1/?uri=spotify:artist:${ARTIST_ID}&size=detail&theme=dark`}
+      className={"spotifyFollow"}
+      scrolling="no"
+      frameBorder="0"
+      style={{ border: "none", overflow: "hidden" }}
+      allowTransparency="true"
+    ></iframe>
+    <SpotifyPlayer
+      theme="white"
+      view="coverart"
+      uri={`spotify:album:3MctXvWGZKZQDmSheN8Cgo`}
+    />
+  </section>
+);
+
+const initialLocation = window.location.href.split("/").pop() || "home";
 
 const App = () => {
   const [bodyClass, setBodyClass] = useState(initialLocation);
-  const CustomLink = ({children, to, bodyClass = ''}) => (
+  const CustomLink = ({ children, to, bodyClass = "" }) => (
     <Link to={to} onClick={() => setBodyClass(bodyClass)}>
       {children}
     </Link>
   );
 
   useEffect(() => {
-    VideoList.forEach(({id}) => {
+    VideoList.forEach(({ id }) => {
       const img = new Image();
       img.src = imageForVideo(id);
     });
@@ -85,16 +95,11 @@ const App = () => {
     <div className={`App ${bodyClass}`}>
       <div className="header">
         <h3>
-          <CustomLink to={`/`}>
-            The Scratch
+          <CustomLink to={`/`} bodyClass={`home`}>
+            THE SCRATCH
           </CustomLink>
         </h3>
         <ul>
-          <li>
-            <a href="https://thescratchmerch.bigcartel.com/products" target="_blank">
-              Merch
-            </a>
-          </li>
           <li>
             <CustomLink to={`/gigs`} bodyClass={`gigs`}>
               Gigs
@@ -113,21 +118,42 @@ const App = () => {
         </ul>
       </div>
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/merch' component={Merch}/>
-        <Route exact path='/merchandise' component={Merchandise}/>
-        <Route path='/gigs' component={Gigs}/>
-        <Route path='/videos' component={VideoSection}/>
-        <Route path='/music' component={Music}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/merch" component={Merch} />
+        <Route exact path="/merchandise" component={Merchandise} />
+        <Route path="/gigs" component={Gigs} />
+        <Route path="/videos" component={VideoSection} />
+        <Route path="/music" component={Music} />
       </Switch>
       <div className="footer">
         <div id="logo_section">
-          <Bird/>
+          <Bird />
         </div>
         <div id="social_icons">
-          <IconLink link="https://www.facebook.com/TheScratchMusic/" icon="fa fa-facebook"/>
-          <IconLink link="https://www.instagram.com/thescratchmusic/" icon="fa fa-instagram"/>
-          <IconLink link="https://www.youtube.com/channel/UCBNB4bpnfLTnxVJKrLUi0ow" icon="fa fa-youtube"/>
+          <IconLink
+            link="https://open.spotify.com/album/3MctXvWGZKZQDmSheN8Cgo?si=YP73sjryS-O1O5k5qaYxpA"
+            icon="fa fa-spotify"
+          />
+          <IconLink
+            link="https://music.apple.com/ie/album/couldnt-give-a-rats/1503585257"
+            icon="fa fa-apple"
+          />
+          <IconLink
+            link="https://www.instagram.com/thescratchmusic/"
+            icon="fa fa-instagram"
+          />
+          <IconLink
+            link="https://www.facebook.com/TheScratchMusic/"
+            icon="fa fa-facebook"
+          />
+          <IconLink
+            link="https://twitter.com/thescratchmusic"
+            icon="fa fa-twitter"
+          />
+          <IconLink
+            link="https://www.youtube.com/channel/UCBNB4bpnfLTnxVJKrLUi0ow"
+            icon="fa fa-youtube"
+          />
         </div>
         <div id="contact_section">
           <p>Mgmt: anon.musicmanagement@gmail.com</p>
@@ -136,6 +162,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
